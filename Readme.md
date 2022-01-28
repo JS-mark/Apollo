42873a8ad3e5f047685421f8946aad841af41869
The Mark <sunduo3195@qq.com>
2022-01-28 11:05:18 +0800

    feat: init project

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# 简介
+
+- 名称起源 `Apollo` 源于北欧神话太阳神阿波罗的音译！
+
+## 开发
+
+```json
+// pnpm 重写路径
+"overrides": {
+  "vite": "workspace:*",
+  "@vitejs/plugin-vue": "workspace:*"
+}
+```
---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: 'Apollo 前端工程化工具',
+  description: '基于 Vite 的下一代工程脚手架',
+  themeConfig: {
+    // Type is `DefaultTheme.Config`
+  }
+})
